ef9f45af394b00d96d80c26ae8bc44c496651b6e
chimsau <133568572+chimsau@users.noreply.github.com>
2023-05-16 00:50:00 +1000

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qys2l4z907q6zl3e4mwf0gc2xhrrjpr5y8zr6sx4aqfdv66cdd8kzqqr2durd -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qys2l4z907q6zl3e4mwf0gc2xhrrjpr5y8zr6sx4aqfdv66cdd8kzqqr2durd -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
     sleep 5;
 done
